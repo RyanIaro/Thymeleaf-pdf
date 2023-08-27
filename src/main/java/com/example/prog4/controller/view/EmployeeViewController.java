19f82ce4e486891221212d2e4c57ed9c092070e4
RyanIaro <hei.ryan.3@gmail.com>
2023-08-27 19:42:22 +0300

    feat: allow saving employee sheet as pdf

--- src/main/java/com/example/prog4/controller/view/EmployeeViewController.java
+++ src/main/java/com/example/prog4/controller/view/EmployeeViewController.java
@@ -1,10 +1,12 @@
 package com.example.prog4.controller.view;
 
+import com.example.prog4.conf.company.CompanyConf;
 import com.example.prog4.controller.PopulateController;
 import com.example.prog4.controller.mapper.EmployeeMapper;
 import com.example.prog4.model.Employee;
 import com.example.prog4.model.EmployeeFilter;
 import com.example.prog4.service.EmployeeService;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Sort;
@@ -14,11 +16,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import org.xhtmlrenderer.pdf.ITextRenderer;
+
+import java.io.OutputStream;
+import java.io.StringWriter;
 
 @Controller
 @RequestMapping("/employee")
 @AllArgsConstructor
 public class EmployeeViewController extends PopulateController {
+    private final TemplateEngine templateEngine;
     private EmployeeService employeeService;
     private EmployeeMapper employeeMapper;
 
@@ -65,5 +74,31 @@ public class EmployeeViewController extends PopulateController {
     @GetMapping("/")
     public String home() {
         return "redirect:/employee/list";
+    }
+
+    @GetMapping("/show/{eId}/pdf")
+    public void generateEmployeePdf(@PathVariable String eId, HttpServletResponse response) throws Exception {
+        Employee employee = employeeMapper.toView(employeeService.getOne(eId));
+        String cnaps = employeeService.getEmployeeCnaps(eId);
+        CompanyConf company = new CompanyConf();
+
+        Context context = new Context();
+        context.setVariable("employee", employee);
+        context.setVariable("cnaps", cnaps);
+        context.setVariable("company", company);
+
+        StringWriter writer = new StringWriter();
+        templateEngine.process("employee_pdf", context, writer);
+
+        response.setContentType("application/pdf");
+        response.setHeader("Content-Disposition", "attachment; filename=Employee "+eId+".pdf");
+
+        OutputStream outputStream = response.getOutputStream();
+        ITextRenderer renderer = new ITextRenderer();
+        renderer.setDocumentFromString(writer.toString());
+        renderer.layout();
+        renderer.createPDF(outputStream);
+
+        outputStream.close();
     }
 }
